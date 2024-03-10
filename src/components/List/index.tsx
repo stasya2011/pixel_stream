ec7acd969df40ef76272584e6c572faf4f70df93
Stasya Geleisha <s.geleisha@gmail.com>
2024-03-10 22:17:52 +0200

    feat: infinity scroll

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,15 +1,54 @@
 import { useState, useEffect } from "react";
 import { PhotoData } from "../../types";
-import { handler } from "../../utils";
+import { fetchingData } from "../../utils";
 import Image from "../Image";
 import styles from "./list.module.scss";
 
 const List = () => {
   const [listOfImgs, setListOfImgs] = useState<PhotoData[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [fetching, setFetching] = useState<boolean>(false);
 
   useEffect(() => {
-    handler(setListOfImgs);
+    if (localStorage.getItem("pixel_stream")) {
+      const data = localStorage.getItem("pixel_stream");
+      const photos = data ? JSON.parse(data) : null;
+      if (photos && Array.isArray(photos)) {
+        setListOfImgs(photos);
+      }
+      return;
+    } else {
+      fetchingData(setListOfImgs, setFetching, listOfImgs, currentPage);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (fetching) {
+      fetchingData(setListOfImgs, setFetching, listOfImgs, currentPage);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fetching]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = (e: Event) => {
+    const target = e.target as Document;
+    if (
+      target.documentElement.scrollHeight -
+        (target.documentElement.scrollTop + window.innerHeight) <
+      100
+    ) {
+      setFetching(true);
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
+  };
 
   return (
     <div className={styles.wrapper}>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,17 +1,30 @@
 import { PhotoData } from "../types";
 
-export const handler = async (setListOfImgs: (array: PhotoData[]) => void) => {
+export const fetchingData = async (
+  setListOfImgs: (array: PhotoData[]) => void,
+  setFetching: (value: boolean) => void,
+  listOfImgs: PhotoData[],
+  currentPage: number
+) => {
   if (process.env.REACT_APP_API_KEY) {
-    const a = await fetch(
-      "https://api.pexels.com/v1/curated?page=1&per_page=10",
-      {
-        headers: {
-          Authorization: process.env.REACT_APP_API_KEY,
-        },
-      }
-    );
-    const b = await a.json();
-    setListOfImgs(b.photos);
+    try {
+      const response = await fetch(
+        `https://api.pexels.com/v1/curated?page=${currentPage}&per_page=12`,
+        {
+          headers: {
+            Authorization: process.env.REACT_APP_API_KEY,
+          },
+        }
+      );
+      const data = await response.json();
+      setListOfImgs([...listOfImgs, ...data.photos]);
+      localStorage.setItem(
+        "pixel_stream",
+        JSON.stringify([...listOfImgs, ...data.photos])
+      );
+    } finally {
+      setFetching(false);
+    }
   } else {
     console.log("Oooops...", process.env.REACT_APP_API_KEY);
   }
